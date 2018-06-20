9036edc09c3d2c88f65edd10b0cc59a33d200217
Dai Iwabuchi <d.iwabuchi@vanq.in>
2018-06-21 06:16:34 +0900

    npm install and add .editorconfig

--- src/Pagination.js
+++ src/Pagination.js
@@ -26,17 +26,17 @@ module.exports = {
     }
   },
   created: function() {
-    
+
     if (!this.vuex) return;
-    
+
     if (!this.for) {
       throw new Error('vue-pagination-2: The "for" prop is required when using vuex');
     }
-    
+
     let name = this.for;
-    
+
     if (this.$store.state[name]) return;
-    
+
     this.$store.registerModule(this.for,  {
       state: {
         page: 1
@@ -59,30 +59,30 @@ module.exports = {
       return merge(defaultOptions(), this.options);
     },
     Theme() {
-      
+
       if (typeof this.opts.theme==='object') {
         return this.opts.theme;
-      } 
-      
+      }
+
       var themes = {
         bootstrap3:require('./themes/bootstrap3'),
         bootstrap4:require('./themes/bootstrap4'),
-        bulma:require('./themes/bulma')    
+        bulma:require('./themes/bulma')
       }
-      
+
       if (typeof themes[this.opts.theme]===undefined) {
         throw `vue-pagination-2: the theme ${this.opts.theme} does not exist`;
       }
-      
+
       return themes[this.opts.theme];
-    },      
+    },
     page() {
       return this.vuex?this.$store.state[this.for].page:this.Page;
     },
     pages: function() {
       if (!this.records)
       return [];
-      
+
       return range(this.paginationStart, this.pagesInCurrentChunk);
     },
     totalPages: function() {
@@ -95,35 +95,35 @@ module.exports = {
       return Math.ceil(this.page / this.opts.chunk);
     },
     paginationStart: function() {
-      
+
       if (this.opts.chunksNavigation==='scroll') {
         return this.firstPage;
       }
-      
+
       return ((this.currentChunk-1) * this.opts.chunk) + 1;
     },
     pagesInCurrentChunk: function() {
       return this.paginationStart + this.opts.chunk <= this.totalPages?
       this.opts.chunk:
       this.totalPages - this.paginationStart + 1;
-      
+
     },
     count: function() {
-      
-      
+
+
       if (/{page}/.test(this.opts.texts.count)) {
-        
+
         if (this.totalPages<=1) return '';
-        
+
         return this.opts.texts.count.replace('{page}', this.page).replace('{pages}', this.totalPages);
-        
+
       }
-      
+
       let parts = this.opts.texts.count.split('|');
       let from = ((this.page-1) * this.perPage) + 1;
       let to = this.page==(this.totalPages)?this.records:from + this.perPage - 1;
       let i = Math.min(this.records==1?2:this.totalPages==1?1:0, parts.length-1);
-      
+
       return parts[i].replace('{count}', this.formatNumber(this.records))
       .replace('{from}', this.formatNumber(from))
       .replace('{to}', this.formatNumber(to))
@@ -141,34 +141,34 @@ module.exports = {
       } else {
         this.Page = page;
       }
-      
+
       this.$emit('paginate', page);
-      
+
       if (this.for) {
-        bus.$emit('vue-pagination::' + this.for, page);        
+        bus.$emit('vue-pagination::' + this.for, page);
       }
     },
     next: function() {
       var page = this.page + 1;
       if (this.opts.chunksNavigation==='scroll' && this.allowedPage(page) && !this.inDisplay(page)) {
-        this.firstPage++; 
+        this.firstPage++;
       }
       return this.setPage(page);
     },
     prev: function() {
       var page = this.page - 1;
-      
+
       if (this.opts.chunksNavigation==='scroll' && this.allowedPage(page) &&  !this.inDisplay(page)) {
-        this.firstPage--; 
+        this.firstPage--;
       }
-      
+
       return this.setPage(page);
     },
     inDisplay(page) {
-      
+
       var start = this.firstPage;
       var end = start + this.opts.chunk - 1;
-      
+
       return page>=start && page<=end;
     },
     nextChunk: function() {
@@ -197,9 +197,9 @@ module.exports = {
       return this.page==page?this.Theme.active:'';
     },
     formatNumber: function (num) {
-      
+
       if (!this.opts.format) return num;
-      
+
       return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
   },
@@ -215,4 +215,3 @@ function range(start, count) {
     return index + start;
   });
 }
-
